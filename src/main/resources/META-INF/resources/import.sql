689fa2df07343f9e1a4aafd6b538baeb1edee572
mattialoiarro <mattia.loiarro@lernende.bbw.ch>
2022-11-22 15:17:15 +0100

    added import.sql and tested the values

---
+++ src/main/resources/META-INF/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO "Rollen" ("role") VALUES ('Mitglied');
+
+INSERT INTO "Room" ("groesse","sitzplatz","officeart") VALUES ('123','4','Office mit Fenster');
+
+INSERT INTO "User" ("email","password","prename","surname") VALUES ('test@gmail.com','test','Mattia','Loiarro');
+
+INSERT INTO "Booking" ("buchungsnummer","date","ganzertag","status","text") VALUES ('1232832323928','2022-11-22',true,'wird noch aktzeptiert','test');
+
+INSERT INTO "Equipment" ("name","preis") VALUES ('Bildschirm','45');
